a5c18098a208544bbb54f4c722c329c8a75642a9
julioasvn <59871300+julioasvn@users.noreply.github.com>
2021-04-21 09:47:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,317 @@
+var cartaUm = {
+    nome: "Wolverine",
+    imagem: "https://conteudo.imguol.com.br/blogs/88/files/2013/08/wolverine-artwork-wallpapers_36592_1920x1200-1024x640.jpg",
+    atributos: {
+        ataque: 80,
+        defesa: 90,
+        magia: 10
+    }
+}
+var cartaDois = {
+    nome: "Batman",
+    imagem: "https://img.elo7.com.br/product/zoom/256ED02/big-poster-gamer-batman-arkham-asylum-lo01-tam-90x60-cm-geek.jpg",
+    atributos: {
+        ataque: 85,
+        defesa: 70,
+        magia: 20
+    }
+}
+var cartaTres = {
+    nome: "Luke Skywalker",
+    imagem: "https://uploads.spiritfanfiction.com/fanfics/historias/202007/luke-skywalker-legendary-journey-19940117-170720202056.jpg",
+    atributos: {
+        ataque: 90,
+        defesa: 65,
+        magia: 80
+    }
+}
+var cartaQuatro = {
+    nome: "Harry Poter",
+    imagem: "https://media.contentapi.ea.com/content/dam/gin/images/2017/01/harry-potter-goblet-of-fire-key-art.jpg.adapt.crop3x5.533p.jpg",
+    atributos: {
+        ataque: 10,
+        defesa: 30,
+        magia: 80
+    }
+}
+var cartaCinco = {
+    nome: "Saitama",
+    imagem: "https://a-static.mlcdn.com.br/618x463/quadro-decorativo-one-punch-man-anime-desenho-com-moldura-g01-vital-quadros/vitalquadrosdobrasil/90098980550/8868cf84a923421314385bc37c208e0e.jpg",
+    atributos: {
+        ataque: 100,
+        defesa: 100,
+        magia: 10
+    }
+}
+var cartaSeis = {
+    nome: "John Rambo",
+    imagem: "https://i.ytimg.com/an/-T39_xJo9iA/a90a1f89-6fe6-470c-aec6-2fa41c7fc1ab_mq.jpg?v=5ef6178d",
+    atributos: {
+        ataque: 60,
+        defesa: 60,
+        magia: 10
+    }
+}
+var cartaSete = {
+    nome: "Gandalf",
+    imagem: "https://i.pinimg.com/originals/9f/ce/b5/9fceb5d0391ce50f5e439f2299429a43.jpg",
+    atributos: {
+        ataque: 50,
+        defesa: 60,
+        magia: 95
+    }
+}
+var cartaOito = {
+    nome: "Mestre dos Magos",
+    imagem: "https://cdna.artstation.com/p/assets/images/images/022/519/536/large/robson-michel-dungeon-master-1500-3.jpg?1575724730",
+    atributos: {
+        ataque: 10,
+        defesa: 65,
+        magia: 95
+    }
+}
+var cartaNove = {
+    nome: "Drácula",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f7d886e-4b07-4c28-8b49-662532a540c7/ddrqrfg-b0d83df0-bc55-47ab-86b4-6bd23117128c.png/v1/fill/w_1024,h_1025,q_80,strp/dracula_castlevania_by_eltonds_ddrqrfg-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMDI1IiwicGF0aCI6IlwvZlwvNWY3ZDg4NmUtNGIwNy00YzI4LThiNDktNjYyNTMyYTU0MGM3XC9kZHJxcmZnLWIwZDgzZGYwLWJjNTUtNDdhYi04NmI0LTZiZDIzMTE3MTI4Yy5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.QT0ieZhKCQ0MpGWn6vCnNgmEyOkKykk2n0W1ZQh1ztg",
+    atributos: {
+        ataque: 90,
+        defesa: 90,
+        magia: 90
+    }
+}
+var cartaDez = {
+    nome: "Snake Plissken",
+    imagem: "https://i.ebayimg.com/images/g/5AgAAOSwgQ9V4fJf/s-l300.jpg",
+    atributos: {
+        ataque: 60,
+        defesa: 65,
+        magia: 20
+    }
+}
+var cartaOnze = {
+    nome: "Elric de Melniboné",
+    imagem: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/hostedimages/1429718652i/14623644._SY540_.jpg",
+    atributos: {
+        ataque: 25,
+        defesa: 25,
+        magia: 90
+    }
+}
+var cartaDoze = {
+    nome: "Conan da Ciméria",
+    imagem: "https://i.pinimg.com/originals/18/d5/3e/18d53e7380cc710838e1f2ec4fd100b3.jpg",
+    atributos: {
+        ataque: 80,
+        defesa: 80,
+        magia: 10
+    }
+}
+var cartaTreze = {
+    nome: "Rick Grimes",
+    imagem: "https://images-na.ssl-images-amazon.com/images/I/51XDY8Dd6GL._AC_.jpg",
+    atributos: {
+        ataque: 50,
+        defesa: 50,
+        magia: 5
+    }
+}
+var cartaQuatorze = {
+    nome: "Michonne",
+    imagem: "https://i.pinimg.com/originals/8d/02/87/8d0287ee7cf06938c1535eb752ed0b6b.jpg",
+    atributos: {
+        ataque: 60,
+        defesa: 40,
+        magia: 5
+    }
+}
+var cartaQuinze = {
+    nome: "Daryl Dixon",
+    imagem: "https://upload.wikimedia.org/wikipedia/pt/f/f9/Norman_Reedus_as_Daryl_Dixon.png",
+    atributos: {
+        ataque: 45,
+        defesa: 45,
+        magia: 10
+    }
+}
+var cartaDezesseis = {
+    nome: "Xenomorfo",
+    imagem: "https://i.pinimg.com/originals/76/e1/a4/76e1a4a82a786a1999354ae5a52f7d3a.png",
+    atributos: {
+        ataque: 90,
+        defesa: 90,
+        magia: 10
+    }
+}
+var cartaDezessete = {
+    nome: "Predador",
+    imagem: "https://i.pinimg.com/originals/92/46/d7/9246d78a4fee25d9d796bf3a78b85e26.jpg",
+    atributos: {
+        ataque: 80,
+        defesa: 70,
+        magia: 20
+    }
+}
+var cartaDezoito = {
+    nome: "Peter",
+    imagem: "https://i.dailymail.co.uk/i/newpix/2018/04/19/16/4B50E40400000578-5634961-_You_re_in_Deadpool_2_from_the_studio_that_killed_Wolverine_hits-a-27_1524153026322.jpg",
+    atributos: {
+        ataque: 10,
+        defesa: 10,
+        magia: 0
+    }
+}
+var cartaDezenove = {
+    nome: "Terminator",
+    imagem: "https://m.media-amazon.com/images/I/51UrXIxrJsL.jpg",
+    atributos: {
+        ataque: 85,
+        defesa: 85,
+        magia: 20
+    }
+}
+var cartaNoventaENove = {
+    nome: "Cthulhu - SUPER TRUNFO!",
+    imagem: "https://i.pinimg.com/originals/c7/15/97/c7159793f9f474b93439719fba84906e.jpg",
+    atributos: {
+        ataque: 999,
+        defesa: 999,
+        magia: 999
+    }
+}
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaUm, cartaDois, cartaTres, cartaQuatro, cartaCinco, cartaSeis, cartaSete, cartaOito, cartaNove, cartaDez, cartaOnze, cartaDoze, cartaTreze, cartaQuatorze, cartaQuinze, cartaDezesseis, cartaDezessete, cartaDezoito, cartaDezenove, cartaNoventaENove]
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+function atualizaQuantidadeDeCartas(){
+  var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+  var html = "Cartas no baralho: " + cartas.length
+  
+  divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar(){
+  var divPlacar = document.getElementById('placar')
+  var html = "Jogador " + pontosJogador + " X " + pontosMaquina + " Máquina"
+  divPlacar.innerHTML = html
+}
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
+  
+    //var numeroCartaJogador = 7
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    
+    cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador, 1)
+    
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+  
+    exibeCartaJogador()
+    
+}
+
+function exibeCartaJogador(){
+  var divCartaJogador = document.getElementById("carta-jogador")
+  var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">'  
+  divCartaJogador.style.backgroundImage=`url(${cartaJogador.imagem})`
+  var nome = `<p class='carta-subtitle'>${cartaJogador.nome}</p>`  
+  var opcoesTexto = ""
+    
+  for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+  }
+  
+  
+  var html = "<div id='opcoes' class='carta-status'>"
+  
+  divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + "</div>"
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        //alert('Venceu a carta máquina')
+        htmlResultado = '<p class="resultado-final">VENCEU!</p>'
+        pontosJogador++
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+      
+        htmlResultado = '<p class="resultado-final">PERDEU!</p>'
+        pontosMaquina++
+    } else {
+      
+      htmlResultado = '<p class="resultado-final">EMPATOU!</p>'
+    }
+  
+  exibeCartaMaquina()
+  
+  if(cartas.length == 0){
+    alert("Fim de Jogo!")
+    if(pontosJogador > pontosMaquina){
+      htmlResultado = '<p class="resultado-final">O HUMANO VENCEU!</p>'
+    }else if(pontosJogador < pontosMaquina){
+      htmlResultado = '<p class="resultado-final">A MÁQUINA VENCEU!</p>'
+    }else{
+      htmlResultado = '<p class="resultado-final">A PARTIDA EMPATOU!</p>'
+    }
+  }else{
+     document.getElementById('btnProximaRodada').disabled = false  
+  }
+  
+  divResultado.innerHTML = htmlResultado 
+  document.getElementById('btnJogar').disabled = true
+  
+  atualizaPlacar()
+  atualizaQuantidadeDeCartas()
+  
+}
+
+function exibeCartaMaquina(){
+  
+  var divCartaMaquina = document.getElementById("carta-maquina")
+  var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">'
+      
+  divCartaMaquina.style.backgroundImage=`url(${cartaMaquina.imagem})`
+  var nome = `<p class='carta-subtitle'>${cartaMaquina.nome}</p>`
+  
+  var opcoesTexto = ""
+    
+  for (var atributo in cartaMaquina.atributos) {
+     opcoesTexto += "<p type='txt' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+  }
+      
+  var html = "<div id='opcoes' class='carta-status'>"
+  
+  divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + "</div>"
+} 
+
+function proximaRodada(){
+  var divCartas = document.getElementById('cartas')
+  divCartas.innerHTML = `<div id = "carta-jogador" class = "carta"></div> <div id = "carta-maquina" class = "carta"></div>`
+  document.getElementById('btnSortear').disabled = false
+  document.getElementById('btnJogar').disabled = true
+  document.getElementById('btnProximaRodada').disabled = true
+  var divResultado = document.getElementById('resultado')
+  divResultado.innerHTML = ""
+}
